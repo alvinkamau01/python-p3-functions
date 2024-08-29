e907285e770f33983c66e06a1fac452d069b0531
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-08-29 22:49:41 +0300

    .

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+     print("Hello, programmer!")
+
+name='Guido'
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!") 
+    return
+
 
 def greet_with_default(name="programmer"):
-    pass
+     print(f"Hello, {name}!")
+     return
 
 def add(num1, num2):
-    pass
-
+    return num1 + num2
+     
 def halve(number):
-    pass
+    if not isinstance(number, (int,float)):  # Check if the number is of type int
+        return None
+    else:
+        return number / 2
+
